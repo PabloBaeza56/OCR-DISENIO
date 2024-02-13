e7f4fde67f155c22307d7ee63678e057c0ac2409
Pablo Baeza <pabloten56@hotmail.com>
2024-02-13 17:49:35 -0600

    Avance

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,33 +9,21 @@ import net.sourceforge.tess4j.TesseractException;
 public class Main {
     public static void main(String[] args) throws TesseractException, IOException {
         
-       /*
+       
         ManejoImagenes jj = new ManejoImagenes();
        
        
         jj.EncontrarContornos("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivo.png", "C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivoSALIDA.png");
-       // 
         jj.DividirImagenesPorContorno("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivoSALIDA.png");
-       
-        System.out.println(jj.detectarRostros("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivoSALIDA.pngXXXX0.png"));
-        //
-        
-        System.out.println(jj.coordenadasSet);
-        //CoordenadasRojas salidax = jj.VerificarContencion();
-        //System.out.println(salidax);
-        
-        //System.out.println(jj.coordenadasSet);
+        jj.detectarRostros("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivoSALIDA.pngXXXX0.png");
         List<CoordenadasRojas> listaCoordenadas = new ArrayList<>(jj.coordenadasSet);
         jj.RecortarImagen("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivo.png",listaCoordenadas.get(0));
-        */
-        
-        
         
         
         OCR conti = new OCR();
-        String salida = (conti.MetodoAlternativo("C:\\Users\\pablo\\OneDrive\\Escritorio\\SALIDAPNG\\definitivo.png"));
+        String salida = (conti.MetodoAlternativo("C:\\Users\\pablo\\OneDrive\\Escritorio\\img20240209_18444422.png"));
         System.out.println(salida);
-        /*
+        
         PersonaINE ciudadano = new PersonaINE();
         ManejoCadenas manejador = new ManejoCadenas(salida, ciudadano);
         manejador.GuardarSubcadenasConMasDeUnDigito(manejador.LimpiarCadenaValoresBasura());
@@ -50,22 +38,12 @@ public class Main {
         manejador.EncontrarDomicilio();
         System.out.println(ciudadano);
         System.out.println("Cadenas no analizadas " + manejador.DevolverElementosNoIdentificados());
-        */
+        
         
        
        
         
-        /*
-        manejador.BuscarCURPyClaveElector();
-        manejador.EncontrarClaveElector();
-        manejador.EncontrarNombres();
-        manejador.EncontrarEntidadFederativa();
-        manejador.EncontrarSexo();
-        manejador.EncontrarFechaNacimiento();
-        manejador.EncontrarDomicilio();
-        System.out.println(ciudadano);
-        System.out.println("Cadenas no analizadas " + manejador.DevolverElementosNoIdentificados());
-        */
+       
         
         
     }
